c8c4394da2b27e9cacaf95ffcba5dac0c2095f22
DanielCancelas <dcancelasgarcia@danielcastelao.org>
2019-10-04 11:01:38 +0200

    update

--- src/bol1/Bol1.java
+++ src/bol1/Bol1.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Bol1 {
 
     public static void main(String[] args) {
-        System.out.println("");
+        /* System.out.println("");
 
-        //CALCULA O ÁREA DUN TRIÁNGULO DE BASE 4 E ALTURA 3
+        //1-CALCULA O ÁREA DUN TRIÁNGULO DE BASE 4 E ALTURA 3
             int base=4,altura=3,area;
             area=(base*altura)/3;
-            System.out.println("A área do triángulo é: "+area);
+            System.out.println("A área do triángulo é: "+area); */
 
-        System.out.println("");
-        //CALCULA LONXITUDE CIRCUNFERENCIA
+        /* System.out.println("");
+        //2-CALCULA LONXITUDE CIRCUNFERENCIA
             /* float radio=2.5f, lonxitude;
             lonxitude=2*3.14f*radio;
             System.out.println("A lonxitude da circunferencia é: "+lonxitude); */
@@ -31,19 +31,48 @@ public class Bol1 {
             float lonxitude=(float)(2*Math.PI*radio);
             System.out.println("A lonxitude da circunferencia é: "+lonxitude); */
         //Pedir datos por teclado
-            Scanner sc=new Scanner(System.in);
+            /* Scanner sc=new Scanner(System.in);
                 System.out.println("Introduce el radio:");
                 float radio=sc.nextFloat();
                 sc.close();
-            System.out.println("A lonxitude da circunferencia é: "+(2*Math.PI*radio));
+            System.out.println("A lonxitude da circunferencia é: "+(2*Math.PI*radio)); */
         
-        System.out.println("");
-        //PASAR DE EUROS A DÓLARES
+        /* System.out.println("");
+        //3-PASAR DE EUROS A DÓLARES
             Scanner cambio=new Scanner(System.in);
-                System.out.printIn("Introduce una cantidad en euros:");
+                System.out.println("Introduce una cantidad en euros:");
                 float euro=cambio.nextFloat();
                 cambio.close();
             float dolar=euro*1.09f;
-            System.out.println("El cambio en dólares es: "+dolar+"$");
+            System.out.println("El cambio en dólares es: "+dolar+"$"); */
+
+        /* System.out.println("");
+        //4-SUMA,RESTA,PRODUTO E COCIENTE
+            Scanner cifras=new Scanner(System.in);
+                System.out.println("Introduce un número:");
+                int n1=cifras.nextInt();
+                System.out.println("Introduce otro número:");
+                int n2=cifras.nextInt();
+                cifras.close();
+            int s=(n1+n2);
+                System.out.print("O resultado da suma é: "+s);
+                System.out.println("");
+            int r=(n1-n2);
+                System.out.print("O resultado da resta é: "+r);
+                System.out.println("");
+            int p=(n1*n2);
+                System.out.print("O resultado do producto é: "+p);
+                System.out.println("");
+            int c=(n1/n2);
+                System.out.println("O resultado do cociente é: "+c); */
+
+        System.out.println("");
+        //5-PASAR DE MILLAS A METROS
+            Scanner distancia=new Scanner(System.in);
+                System.out.println("Introduce una distancia en millas:");
+                float milla=distancia.nextFloat();
+                distancia.close();
+            float metro=milla*1852f;
+            System.out.println("La distancia en metros es: "+metro+" m");
     }
 }
